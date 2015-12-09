6c2ac6eb61a2221cafc36aee260263dac34a4dea
Huaming Rao <huaming.rao@gmail.com>
2015-12-09 05:43:54 +0000

    direct call forever instead of exec forever

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ set -e
 APP_DIR=/app
 
 if [ ! -d ${APP_DIR}/bundle ]; then
-  exec /build.sh
+  /bin/bash /build.sh
 fi
 
 APP_DIR=${APP_DIR}/bundle
 # Run meteor
 cd ${APP_DIR}
 echo "Starting Meteor..."
-exec forever start --uid image-transcription --append --minUptime 1000 --spinSleepTime 1000 main.js && forever logs -f 0
+forever start --uid image-transcription --append --minUptime 1000 --spinSleepTime 1000 main.js && forever logs -f 0
